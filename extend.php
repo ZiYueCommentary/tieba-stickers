e744733db850ec66fd64373243664122e09f9fc4
子悦解说 <ziyuecommentary@gmail.com>
2022-11-28 17:38:52 +0800

    More stickers

--- extend.php
+++ extend.php
@@ -2,10 +2,19 @@
 use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
-return (new Extend\Formatter)
+var img:string = '<img class="tieba_stickers" src="/assets/extensions/wvbdev-tieba-stickers/{TEXT1}.png" width="30px"/>';
+
+return [
+    (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
-                '[tieba={TEXT1}]',
-                '<img class="tieba_stickers" src="/assets/extensions/wvbdev-tieba-stickers/{TEXT1}.png" width="30px"/>',
+                '[tieba={TEXT1}]', img
             );
-        });
+        }),
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config){
+            $config->BBCodes->addCustom(
+                '[tieba]{TEXT1}[/tieba]', img
+            );
+        })
+    ];
